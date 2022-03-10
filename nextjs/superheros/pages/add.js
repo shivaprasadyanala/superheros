ab59f63aa69935acf4770ea92a79a68c57c0be60
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-03-10 07:59:42 +0530

    changed component name

--- nextjs/superheros/pages/add.js
+++ nextjs/superheros/pages/add.js
@@ -4,56 +4,59 @@ import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { useState } from 'react';
 
-function addNewHero() {
- const [form, setForm] = useState({
-  superHero: '',
-  realName: ''
- })
- const handleChange = (e) => {
-  setForm({
-   ...form,
-   [e.target.name]: e.target.value
-  })
- }
- const handleForm = async (e) => {
-  e.preventDefault();
-  try {
-   const res = await axios('http://localhost:3000/api/hero', {
-    method: "POST",
-    headers: {
-     "Content-Type": "application/json"
-    },
-    data: JSON.stringify(form)
-   })
-   Router.push("/")
-  } catch (error) {
+function AddNewHero() {
+    const [form, setForm] = useState({
+        superHero: '',
+        realName: ''
+    })
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleForm = async(e) => {
+        e.preventDefault();
+        try {
+            const res = await axios('http://localhost:3000/api/hero', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(form)
+            })
+            Router.push("/")
+        } catch (error) {
 
-  }
- }
- return (
-  <div className='container'>
-   <h1 className="display-3">
-    add a new hero identity
-   </h1>
-   <form onSubmit={handleForm}>
-    <MDBInput
-     onChange={handleChange}
-     label='SuperHero'
-     type="text"
-     name='superHero'
+        }
+    }
+    return ( <
+        div className = 'container' >
+        <
+        h1 className = "display-3" >
+        add a new hero identity <
+        /h1> <
+        form onSubmit = { handleForm } >
+        <
+        MDBInput onChange = { handleChange }
+        label = 'SuperHero'
+        type = "text"
+        name = 'superHero'
 
-    ></MDBInput>
-    <MDBInput className='my-2'
-     onChange={handleChange}
-     label='realName'
-     type="text"
-     name='realName'
+        >
+        < /MDBInput> <
+        MDBInput className = 'my-2'
+        onChange = { handleChange }
+        label = 'realName'
+        type = "text"
+        name = 'realName'
 
-    ></MDBInput>
-    <MDBBtn type='submit'>Add new hero</MDBBtn>
-   </form>
-  </div>
- )
+        >
+        < /MDBInput> <
+        MDBBtn type = 'submit' > Add new hero < /MDBBtn> <
+        /form> <
+        /div>
+    )
 }
 
 export default addNewHero
